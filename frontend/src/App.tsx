da7518db23bd8c087076beffbbeeb123102f1889
hanish <hkavalancheryha8688@conestogac.on.ca>
2024-09-21 12:48:57 -0400

    (ui)dark-mode/light-mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Button } from './components/ui/button'
 import { LoginForm } from './pages/login'
 import { SignUpForm } from "./pages/signup";
 import { NotFound } from "./pages/fallback";
+import { ThemeProvider } from "./components/ui/theme-provider";
+import { ModeToggle } from "./components/ui/mode-toggle";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<LoginForm />}></Route>
-        <Route path="/signup" element={<SignUpForm />}></Route>
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+      <Routes>
+        <Route path="/">
+          <Route index element={<LoginForm />}></Route>
+          <Route path="/signup" element={<SignUpForm />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ThemeProvider>
 
   )
 }
