d34e82c1b8195e22ee14958d881b6a0a4a83167a
hanish <hkavalancheryha8688@conestogac.on.ca>
2024-09-21 13:36:34 -0400

    home page

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,10 @@
+import Header from "./commons/header";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <Header />
+            <p>Main Page</p>
+        </div>
+    );
+}
