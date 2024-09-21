b46ef8e3c22229837018ff832020959f502419ce
hanish <hkavalancheryha8688@conestogac.on.ca>
2024-09-21 16:39:24 -0400

    wrapper main app

---
+++ frontend/src/pages/main.tsx
@@ -0,0 +1,11 @@
+import Header from "./commons/header";
+import Home from "./home";
+
+export default function MainApp() {
+    return (
+        <div className="main-app">
+            <Header />
+            <Home />
+        </div>
+    );
+}
