eeb8506fb97f933ea0a8e54718751a408be4ca9f
hanish <hkavalancheryha8688@conestogac.on.ca>
2024-09-22 12:47:13 -0400

    adjust for dark/light theme

--- frontend/src/pages/commons/featured_products.tsx
+++ frontend/src/pages/commons/featured_products.tsx
@@ -13,9 +13,9 @@ export default function FeaturedProducts() {
     // aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80
     // h-full w-full object-cover object-center lg:h-full lg:w-full
     return (
-        <div className="bg-white">
+        <div className="w-full border-b md:border-0 rotate-0 scale-100 transition-all dark:-rotate-0 dark:scale-100">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <h2 className="text-2xl font-bold tracking-tight text-gray-900">Best Products For You!</h2>
+                <h2 className="text-2xl font-bold tracking-tight">Best Products For You!</h2>
 
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
@@ -32,13 +32,13 @@ export default function FeaturedProducts() {
                                 </div>
                                 <div className="mt-4 flex justify-between">
                                     <div>
-                                        <h3 className="text-sm text-gray-700">
+                                        <h3 className="text-sm">
                                             {/* <span aria-hidden="true" className="absolute inset-0" /> */}
                                             {product.name}
                                         </h3>
-                                        <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                        <p className="mt-1 text-sm">{product.color}</p>
                                     </div>
-                                    <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                                    <p className="text-sm font-medium">{product.price}</p>
                                 </div>
                             </a>
                         </div>
