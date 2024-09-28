b179901560588b712f8d85805262adb82c77b5cc
hanish <hkavalancheryha8688@conestogac.on.ca>
2024-09-28 15:38:47 -0400

    refactor, conditional header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import { LoginForm } from './pages/login'
 import { SignUpForm } from "./pages/signup";
@@ -13,10 +13,18 @@ import Home from "./pages/home";
 
 function App() {
   const [token, setToken] = useState("");
+  const location = useLocation();
+  console.log({ location });
+
+  // check if the path is allowed to show header and footer
+  // TODO: think of a better way to show header and footer
+  const is_allowed = (path: string) => {
+    return path === "/";
+  }
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <ModeToggle />
-      { token &&  <Header callback={(t: string) => setToken(t)} /> }
+      { (token || is_allowed(location.pathname)) &&  <Header callback={(t: string) => setToken(t)} /> }
       <Routes>
         <Route path="/">
           <Route index element={<Home />}></Route>
@@ -25,7 +33,7 @@ function App() {
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-      { token &&  <Footer /> }
+      { (token || is_allowed(location.pathname)) &&  <Footer /> }
     </ThemeProvider>
 
   )
