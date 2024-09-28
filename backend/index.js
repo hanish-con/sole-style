44e1da4791eeff1ae042fa3b15e5a5782e7cae5e
hanish <hkavalancheryha8688@conestogac.on.ca>
2024-09-27 20:41:28 -0400

    add user registration api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
 import express from "express";
 import {} from "./models/db.js";
+import { User } from "./models/schema.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.json({ message: "hello from app root"});
+});
+
+app.post("/register", async (req, res) => {
+    const user = await User.findOne({ email });
+    if (user) {
+        return res.status(400).json({ error: "User already exists"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+        ...req.body,
+        password: hashedPassword,
+    });
+    newUser.save();
+    return res.send(201).json({ message: "User registered successfully"});
 });
 
 const PORT = process.env.PORT || 3002;
