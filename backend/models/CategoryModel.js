0f69ef5758090faf5a83690ac6f512029e80381a
yash <yash1899610@gmail.com>
2024-10-29 16:46:05 -0400

    Add subcategories

--- backend/models/CategoryModel.js
+++ backend/models/CategoryModel.js
@@ -2,27 +2,41 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+// Define subcategory schema
+const subcategorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
 
+// Define main category schema
 const categorySchema = new Schema({
   name: {
     type: String,
     required: true,
     trim: true,
-    unique: true
+    unique: true,
   },
   description: {
     type: String,
-    required: true
+    required: true,
   },
+  subcategories: [subcategorySchema], 
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   updatedAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-
+// Export the Category model
 export const Category = mongoose.model('Category', categorySchema);
