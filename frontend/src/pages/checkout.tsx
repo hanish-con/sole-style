8ea72cf2ce50b3ea798f11e2ebebd7afbec50022
yash <yash1899610@gmail.com>
2024-11-12 18:24:28 -0500

    Checkout Page

---
+++ frontend/src/pages/checkout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Checkout: React.FC = () => {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <p>Here you can proceed with payment and finalize your order.</p>
+    </div>
+  );
+};
+
+export default Checkout;
